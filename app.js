1c74ecfec9baad758892291e7216d8cdc35807b6
Arron <arronzhang@me.com>
2015-04-05 11:56:23 +0800

    Add proxy.pac

--- app.js
+++ app.js
@@ -17,14 +17,27 @@ http.createServer(function (req, res) {
 	if(host.indexOf("sohu.com") != -1){
 		res.write('<?xml version="1.0" encoding="UTF-8"?><VAST version="3.0"></VAST>');
 		res.end();
+		return;
 	}
 	if(host.indexOf("youku.com") != -1){
 		res.write('{"P": 11,"RSALL": "","VAL": []}');
 		res.end();
+		return;
 	}
 	if(host.indexOf("qq.com") != -1){
 		res.write('<?xml version="1.0" encoding="Utf-8"?><root decs="video-web"></root>');
 		res.end();
+		return;
+	}
+	if(req.url == "/proxy.pac"){
+		var path = "." + req.url;
+		var stat = fs.statSync(path);
+		res.writeHead(200, {
+			'Content-Type': 'text/javascript',
+			'Content-Length': stat.size
+		});
+		fs.createReadStream(path).pipe(res);
+		return;
 	}
 	if(req.url == "/youku.mp4"){
 		var path = "." + req.url;
